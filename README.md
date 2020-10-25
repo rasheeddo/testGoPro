54ffa49f77e19e25ee4dcfb3d2f4bbf3ec7a085d
rasheeddo <rasheedo.kit@gmail.com>
2020-10-25 17:04:44 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# test GoPro camera
+
+This is just a test script to access GoPro camera and using OpenCV to display the frames.
+
+The GoPro camera API can be from https://github.com/KonradIT/gopro-py-api . This is not an official API from GoPro, but the developers there are doing a very nice API for us to help controlling GoPro to do some automation stuff.
+
+Make sure you install their latest API, at the time I am writing this, it's 4.1.0, so please so `pip install goprocam==4.1.0` before using it.
+
+## My test scripts
+
+`streamGoPro.py` will need the GoPro Wifi to be turned on, this is quite not useful because you need a mobile phone and GoPro app to enable GoPro wifi first (I am using GoPro HERO7 and 9, and this step is needed, let me know how's the other version). The code is pretty much comes from this guy https://youtu.be/Wi6aMYFSwcA , but I was playing around and modified a bit.
+
+`testGoProUSB.py` is to use GoPro as webcam, your PC will join the GoPro network from USB, so make sure your ethernet interface name is correct before using (mine is enp0s20f0u4). Make sure your GoPro is in "GoPro Connect" when USB is plugging. I am using this method for other application like object detection from pydarknet https://github.com/rasheeddo/pydarknet .
---
+++ testGoProUSB.py
@@ -0,0 +1,41 @@
+## the goprocam python API module and example can be found from
+## https://github.com/KonradIT/gopro-py-api
+
+## we just plug the USB directly to pc
+## make sure the gopro is using GoPro Connect, not MTP
+
+import cv2
+import sys
+from goprocam import GoProCamera, constants
+import time
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+## make sure th ethernet inteface name is correctly
+## when plugging USB from gopro to pc, the gopro will works as DHCP server
+gopro = GoProCamera.GoPro(ip_address=GoProCamera.GoPro.getWebcamIP("enp0s20f0u4"),camera=constants.gpcontrol, webcam_device="enp0s20f0u4")
+gopro.webcamFOV(constants.Webcam.FOV.Wide)
+
+## we use gopro as webcam with slightly low resolution, or can try with 1080 as well
+gopro.startWebcam(resolution="480")
+
+## using livestream will end up at error after some times
+# gopro.livestream("start")
+
+# gopro.video_settings(res='480p', fps='30')
+# gopro.gpControlSet(constants.Stream.WINDOW_SIZE, constants.Stream.WindowSize.R720)
+cap = cv2.VideoCapture("udp://172.26.174.51:8554", cv2.CAP_FFMPEG)   # , cv2.CAP_FFMPEG
+t = time.time()
+while True:
+	
+	nmat, frame = cap.read()
+	cv2.imshow("GoPro OpenCV", frame)
+	print(frame.shape)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+# When everything is done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
