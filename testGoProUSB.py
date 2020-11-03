c79e8623fae382a38b83ee5c466003da00980fb2
rasheeddo <rasheedo.kit@gmail.com>
2020-11-04 00:34:18 +0900

    when USB connected, use constant IP

--- testGoProUSB.py
+++ testGoProUSB.py
@@ -14,11 +14,12 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 ## make sure th ethernet inteface name is correctly
 ## when plugging USB from gopro to pc, the gopro will works as DHCP server
-gopro = GoProCamera.GoPro(ip_address=GoProCamera.GoPro.getWebcamIP("enp0s20f0u4"),camera=constants.gpcontrol, webcam_device="enp0s20f0u4")
+# gopro = GoProCamera.GoPro(ip_address=GoProCamera.GoPro.getWebcamIP("enp0s20f0u1"),camera=constants.gpcontrol, webcam_device="enp0s20f0u4")
+gopro = GoProCamera.GoPro(ip_address="172.26.174.51")
 gopro.webcamFOV(constants.Webcam.FOV.Wide)
 
 ## we use gopro as webcam with slightly low resolution, or can try with 1080 as well
-gopro.startWebcam(resolution="480")
+gopro.startWebcam(resolution="1080")   #480
 
 ## using livestream will end up at error after some times
 # gopro.livestream("start")
@@ -31,6 +32,7 @@ while True:
 	
 	nmat, frame = cap.read()
 	cv2.imshow("GoPro OpenCV", frame)
+	# frame = cv2.resize(frame, (1696, 960))
 	print(frame.shape)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
